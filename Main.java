7642977638ea3839198b1722396e7f6e183152c3
F1rell <ratmir777mustafin@mail.ru>
2021-12-26 19:46:45 +0700

    complete

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 52; // баланс счёта
+        int refill = 1001; // сумма пополнения
+        int refillBonus = refill / balance; // кол-во бонусов за внесённую сумму
+        int balancePlusBonus = refillBonus + refill + balance; // баланс после пополнения с бонусами
+        int refillNoBonus = balance + refill; // баланс после пополнения без бонусов
+
+        if (refill > 1000) {
+            System.out.println("Ваш баланс " + balancePlusBonus);
+        } else {
+            System.out.println("Ваш баланс " + refillNoBonus);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете превысила ли
+        // сумма пополнения порог и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
+}
