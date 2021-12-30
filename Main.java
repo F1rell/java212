6a7fdea82553696c4d248c2c439b775dc6b0c1fb
F1rell <ratmir777mustafin@mail.ru>
2021-12-30 14:21:03 +0700

    complete final format

--- Main.java
+++ Main.java
@@ -1,20 +1,14 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-
         int balance = 52; // баланс счёта
-        int refill = 1000; // сумма пополнения
+        int refill = 1001; // сумма пополнения
         int refillBonus = refill / balance; // кол-во бонусов за внесённую сумму
         int balancePlusBonus = refillBonus + refill + balance; // баланс после пополнения с бонусами
         int refillNoBonus = balance + refill; // баланс после пополнения без бонусов
-
         if (refill > 1000) {
             System.out.println("Ваш баланс " + balancePlusBonus);
         } else {
             System.out.println("Ваш баланс " + refillNoBonus);
-
         }
-
     }
 }
